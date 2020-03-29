9c6eb233030323fdbb13cdf5b9a90e414175817b
Alex <bugrimov_a@bk.ru>
2020-03-30 01:51:33 +0300

    [FRONT-#3] Added pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Switch,} from "react-router-dom";
+import Home from './pages/Home';
+import Counts from './pages/Counts';
+import Tariffs from './pages/Tariffs';
+import Payments from './pages/Payments';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
-    <div className="App">
-      Съем квартиры
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/counts">Счетчики</Link>
+        </li>
+        <li>
+          <Link to="/tariffs">Тарифы</Link>
+        </li>
+        <li>
+          <Link to="/payments">Оплата</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/counts" component={Counts}/>
+        <Route path="/tariffs" component={Tariffs}/>
+        <Route path="/payments" component={Payments}/>
+        <Route component={NoMatch}/>
+      </Switch>
+    </Router>
   );
 }
 
