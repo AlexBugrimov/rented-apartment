1d785bc4e796038d4df8299556eb283894731599
Alex <bugrimov_a@bk.ru>
2020-04-30 23:02:31 +0300

    [#8] Added swagger generate

--- backend/src/main/java/dev/bug/backend/BackendApplication.java
+++ backend/src/main/java/dev/bug/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package dev.bug.restapi;
+package dev.bug.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestApiApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
